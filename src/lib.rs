b0d2a2d00ae6924383721f3438515955d74148dd
oyenet1 <bowofadeoyerinde@gmail.com>
2024-11-27 05:42:03 +0100

    constants added to file

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 mod controllers;
 mod models;
 mod routes;
+mod utils;
 mod views;
 
 pub async fn run() {
     let app = routes::create_routes();
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:6000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!(
+        "{}:{}",
+        utils::constants::HOST,
+        utils::constants::PORT
+    ))
+    .await
+    .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
---
+++ src/utils/constants.rs
@@ -0,0 +1,2 @@
+pub const PORT: &str = "6000";
+pub const HOST: &str = "0.0.0.0";
