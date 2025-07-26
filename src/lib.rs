f373ebe9e5f47317ec9d488544ff7579a33bfcba
oyenet1 <bowofadeoyerinde@gmail.com>
2025-07-26 21:14:37 +0100

    setting up files

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+
+pub mod controllers;
+pub mod models;
+pub mod views;
+
+pub async fn run() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(hello_world));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, world!, Welcome to axum apis!"
+}
